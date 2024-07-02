e9671c68e94e6b57476b6818ae8ab2a582a1bc65
ben3dninja <benjamin.lagosanto@gmail.com>
2024-07-02 16:19:36 +0200

    clean up before merge

--- src/bin/client_bevy.rs
+++ src/bin/client_bevy.rs
@@ -3,11 +3,10 @@ use std::{
     net::{IpAddr, Ipv4Addr},
 };
 
-use bevy::app::App;
+use bevy::app::{App, ScheduleRunnerPlugin};
 use rust_space_trading::{
     client_plugin::{ClientNetworkInfo, ClientPlugin},
     engine_plugin::EnginePlugin,
-    gui_plugin::GuiPlugin,
     input_plugin::InputPlugin,
     tui_plugin::{
         search_plugin::SearchPlugin, space_map_plugin::SpaceMapPlugin, tree_plugin::TreePlugin,
@@ -23,6 +22,7 @@ fn main() {
                 server_address: (IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6000),
                 client_address: (IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0),
             }),
+            ScheduleRunnerPlugin::default(),
             EnginePlugin,
             InputPlugin {
                 keymap: get_keymap(env::args()).unwrap(),
@@ -31,7 +31,6 @@ fn main() {
             TreePlugin,
             SpaceMapPlugin,
             SearchPlugin,
-            GuiPlugin,
         ))
         .run();
 }
