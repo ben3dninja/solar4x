fa0c36676421e19ce1fed0b3402e3d7f6b4d02f8
ben3dninja <benjamin.lagosanto@gmail.com>
2024-07-05 09:51:19 +0200

    add basic start menu

---
+++ src/tui_plugin/main_screen.rs
@@ -0,0 +1,125 @@
+use bevy::prelude::*;
+use crossterm::event::KeyEventKind;
+use ratatui::widgets::{List, ListState, StatefulWidget};
+
+use crate::{
+    core_plugin::CoreEvent,
+    keyboard::MainScreenKeymap,
+    utils::{
+        list::{select_next_clamp, select_previous_clamp},
+        ui::Direction2,
+    },
+};
+
+use super::{AppScreen, ChangeAppScreen, ScreenContext};
+
+const SCREENS: [(ChangeAppScreen, &str); 3] = [
+    (ChangeAppScreen::Singleplayer, "Singleplayer"),
+    (ChangeAppScreen::Multiplayer, "Multiplayer"),
+    (ChangeAppScreen::Explorer, "Explore"),
+];
+
+pub struct MainScreenPlugin;
+
+#[derive(Event)]
+pub enum MainScreenEvent {
+    Quit,
+    Select(Direction2),
+}
+
+pub struct MainScreenContext {
+    list_state: ListState,
+}
+
+#[derive(Resource)]
+pub struct MainScreen;
+
+impl Plugin for MainScreenPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<MainScreenEvent>()
+            .insert_resource(MainScreen)
+            .add_systems(Update, handle_main_screen_events);
+    }
+}
+
+impl ScreenContext for MainScreenContext {
+    type ScreenEvent = MainScreenEvent;
+
+    type ScreenKeymap = MainScreenKeymap;
+
+    fn read_input(
+        &mut self,
+        key_event: &bevy_ratatui::event::KeyEvent,
+        keymap: &Self::ScreenKeymap,
+        internal_event: &mut Events<Self::ScreenEvent>,
+    ) -> Option<ChangeAppScreen> {
+        if key_event.kind == KeyEventKind::Release {
+            return None;
+        }
+        use Direction2::*;
+        use MainScreenEvent::*;
+
+        internal_event.send(match key_event {
+            e if keymap.select_next.matches(e) => Select(Down),
+            e if keymap.select_previous.matches(e) => Select(Up),
+            e if keymap.quit.matches(e) => Quit,
+            e if keymap.validate.matches(e) => return Some(self.get_next_screen()),
+            _ => return None,
+        });
+        None
+    }
+}
+impl MainScreenContext {
+    fn get_next_screen(&self) -> ChangeAppScreen {
+        match self.list_state.selected().unwrap() {
+            i if i < SCREENS.len() => SCREENS[i].0,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Default for MainScreenContext {
+    fn default() -> Self {
+        Self {
+            list_state: ListState::default().with_selected(Some(0)),
+        }
+    }
+}
+
+pub fn handle_main_screen_events(
+    mut screen: ResMut<AppScreen>,
+    mut events: EventReader<MainScreenEvent>,
+    mut core_events: EventWriter<CoreEvent>,
+) {
+    if let AppScreen::StartMenu(context) = screen.as_mut() {
+        for event in events.read() {
+            match event {
+                MainScreenEvent::Quit => {
+                    core_events.send(CoreEvent::Quit);
+                }
+                MainScreenEvent::Select(d) => match d {
+                    Direction2::Down => {
+                        select_next_clamp(&mut context.list_state, SCREENS.len() - 1)
+                    }
+                    Direction2::Up => select_previous_clamp(&mut context.list_state, 0),
+                },
+            }
+        }
+    }
+}
+
+impl StatefulWidget for &MainScreen {
+    type State = MainScreenContext;
+
+    fn render(
+        self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+        state: &mut Self::State,
+    ) {
+        let (_, entries): (Vec<_>, Vec<&str>) = SCREENS.into_iter().unzip();
+        List::new(entries)
+            .highlight_symbol(">")
+            .render(area, buf, &mut state.list_state);
+    }
+}
--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,18 +1,20 @@
 use std::{env::Args, error::Error};
 
-use crate::keyboard::Keymap;
+use crate::keyboard::ExplorerKeymap;
 
-pub fn get_keymap(mut args: Args) -> Result<Keymap, Box<dyn Error>> {
-    let mut keymap = Keymap::default();
+pub fn get_keymap(mut args: Args) -> Result<ExplorerKeymap, Box<dyn Error>> {
+    let mut keymap = ExplorerKeymap::default();
 
     if let Some(command) = args.nth(1) {
         match &command[..] {
             "--writekeymap" => {
-                Keymap::default()
+                ExplorerKeymap::default()
                     .write_to_file(args.next().ok_or("Expected output file path")?, false)?;
             }
             "-k" => {
-                keymap = Keymap::from_toml_file(args.next().ok_or("Expected keymap file path")?)?;
+                keymap = ExplorerKeymap::from_toml_file(
+                    args.next().ok_or("Expected keymap file path")?,
+                )?;
             }
             _ => {}
         }
